27a69642104242fa7094044994d3e6a617ae7f1a
Mohamed Elbahja <bm9qdw5r@gmail.com>
2020-04-05 21:18:18 +0100

    init level.

---
+++ src/Level.php
@@ -0,0 +1,11 @@
+<?php
+namespace Teleport\Utils;
+
+class Level
+{
+	public const LOWER 	= 1;
+	public const LOW 	= 3;
+	public const MEDIUM = 5;
+	public const HIGH 	= 7;
+	public const HIGHER = 9;
+}
